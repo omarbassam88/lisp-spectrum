cae1fb48f8f337d9ef360155db55292a081df5a9
Omar Bassam <omar.bassam88@gmail.com>
2024-08-10 19:12:03 +0300

    adding init.el for guile and gambit

---
+++ gambit/init.el
@@ -0,0 +1,31 @@
+;; The Lisp Spectrum Project
+
+;; This is a minimal Emacs configuration to get you started with gambit
+;; You can use this configuration as an example for your own configuration or
+;; you can use this configuration directly by running "emacs -Q -l init.el".
+
+(package-initialize)
+
+(load-theme 'modus-vivendi t)
+(tool-bar-mode -1)
+
+(use-package geiser-gambit
+  :ensure t)
+
+;; Paredit for structural editing
+(use-package paredit
+  :ensure t
+  :hook ((scheme-mode
+	  geiser-repl-mode)
+	 . paredit-mode)
+  :bind
+  (:map paredit-mode-map
+   ;; allow  using of "RET" normally in geiser
+   ("RET" . nil)))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook ((prog-mode
+	  geiser-repl-mode)
+	 . rainbow-delimiters-mode))
+
